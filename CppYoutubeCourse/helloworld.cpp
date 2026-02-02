55a2179178aec2a2fbb5ca801abfd2acca3a6c6c
Pedro Marques <pdro.marqs2015@hotmail.com>
2026-02-01 23:37:12 -0300

    switch case

--- CppYoutubeCourse/helloworld.cpp
+++ CppYoutubeCourse/helloworld.cpp
@@ -311,6 +311,57 @@ int main() {
 
 
 
+	/* >>> ESTRUTURA DE CONTROLE SWITCH CASE
+
+		.Alternativa ao uso de muitos if elses encadeados. Compara um valor com varios casos possiveis.
+
+	*/
+	int month;
+	std::cout << "Enter the month (1-12): ";
+	std::cin >> month;
+
+	switch (month) {
+		case 1:
+			std::cout << "January" << '\n';
+			break;
+		case 2:
+			std::cout << "February" << '\n';
+			break;
+		case 3:
+			std::cout << "March" << '\n';
+			break;
+		case 4:
+			std::cout << "April" << '\n';
+			break;
+		case 5:
+			std::cout << "May" << '\n';
+			break;
+		case 6:
+			std::cout << "June" << '\n';
+			break;
+		case 7:
+			std::cout << "July" << '\n';
+			break;
+		case 8:
+			std::cout << "August" << '\n';
+			break;
+		case 9:
+			std::cout << "September" << '\n';
+			break;
+		case 10:
+			std::cout << "October" << '\n';
+			break;
+		case 11:
+			std::cout << "November" << '\n';
+			break;
+		case 12:
+			std::cout << "December" << '\n';
+			break;
+		default:
+			std::cout << "Please enteder in only numbers (1-12)" << '\n';
+	}
+
+
 
 
 
